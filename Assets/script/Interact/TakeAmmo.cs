49fd8ece409cfefaf924854fffa078f7a2cbbf76
oatlolda <oat.real8@gmail.com>
2026-03-30 21:55:23 +0700

    Add SImple Script

---
+++ Assets/script/Interact/TakeAmmo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeAmmo : MonoBehaviour
+{
+    [SerializeField] private float Ammo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
